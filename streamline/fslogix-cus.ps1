2a59c3d50e8a61790c5c97bd0362542e7f44be1c
Bryan Long <bryan@cloudaiconsulting.io>
2024-03-05 08:27:45 -0500

    updates

--- streamline/fslogix-cus.ps1
+++ streamline/fslogix-cus.ps1
@@ -46,6 +46,18 @@ $name7 = "CloudKerberosTicketRetrievalEnabled"
 $type7 = "DWord"
 $value7 = 1
 
+$name8 = "ProfileType"
+$type8 = "DWord"
+$value8 = 3
+
+$name9 = "PreventLoginWithFailure"
+$type9 = "DWord"
+$value9 = 1
+
+$name10 = "PreventLoginWithTempProfile"
+$type10 = "DWord"
+$value10 = 1
+
 New-Item -Path $path
 New-ItemProperty -Path $path -Name $name -PropertyType $type -Value $value
 New-ItemProperty -Path $path1 -Name $name1 -PropertyType $type1 -Value $value1
@@ -55,3 +67,6 @@ New-ItemProperty -Path $path1 -Name $name4 -PropertyType $type4 -Value $value4
 New-ItemProperty -Path $path1 -Name $name5 -PropertyType $type5 -Value $value5
 New-ItemProperty -Path $path1 -Name $name6 -PropertyType $type6 -Value $value6
 New-ItemProperty -Path $path2 -Name $name7 -PropertyType $type7 -Value $value7
+New-ItemProperty -Path $path1 -Name $name8 -PropertyType $type8 -Value $value8
+New-ItemProperty -Path $path1 -Name $name9 -PropertyType $type9 -Value $value9
+New-ItemProperty -Path $path1 -Name $name10 -PropertyType $type10 -Value $value10
