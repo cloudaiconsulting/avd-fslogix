86681580a9b3d63a7ffefb7e617534ecc521e8cf
bryanlong77 <bryan@cloudaiconsulting.io>
2024-10-22 10:49:43 -0400

    Update fslogix-cus2.ps1

--- streamline/fslogix-cus2.ps1
+++ streamline/fslogix-cus2.ps1
@@ -1,13 +1,13 @@
-$fileexists = Test-Path -Path 'c:\windows\tmp\fslogix.txt'
- If ($fileexists ) {
-    Exit
-}
+# $fileexists = Test-Path -Path 'c:\windows\tmp\fslogix.txt'
+#  If ($fileexists ) {
+#     Exit
+# }
 New-Item -Path "$env:TEMP\FSLogix" -ItemType Directory -ErrorAction SilentlyContinue
 Invoke-WebRequest -Uri https://aka.ms/fslogix_download -OutFile "$env:TEMP\FSLogix\fslogix.zip"
 Expand-Archive "$env:TEMP\FSLogix\fslogix.zip" -DestinationPath "$env:TEMP\FSLogix" -Force
 Start-Process -FilePath "$env:TEMP\FSLogix\x64\Release\FSLogixAppsSetup.exe" -ArgumentList "/install /quiet" -Wait -Passthru
-New-Item -Path "c:\windows\tmp" -ItemType Directory -ErrorAction SilentlyContinue
-New-Item -Path 'c:\windows\tmp\fslogix.txt' -ItemType File -ErrorAction SilentlyContinue
+# New-Item -Path "c:\windows\tmp" -ItemType Directory -ErrorAction SilentlyContinue
+# New-Item -Path 'c:\windows\tmp\fslogix.txt' -ItemType File -ErrorAction SilentlyContinue
 
 
 $path = "HKLM:\SOFTWARE\Policies\Microsoft\AzureADAccount"
