86bb64e63e10d353849c388b71b5107c0ce7164d
bryanlong77 <bryan@cloudaiconsulting.io>
2024-10-23 02:27:37 -0400

    Update fslogix-cus2.ps1

--- streamline/fslogix-cus2.ps1
+++ streamline/fslogix-cus2.ps1
@@ -20,7 +20,7 @@ $value = 1
 
 $name1 = "Enabled"
 $type1 = "DWord"
-$value1 = 1
+$value1 = 0
 
 $name2 = "CCDLocations"
 $type2 = "String"
@@ -52,11 +52,11 @@ $value8 = 0
 
 $name9 = "PreventLoginWithFailure"
 $type9 = "DWord"
-$value9 = 1
+$value9 = 0
 
 $name10 = "PreventLoginWithTempProfile"
 $type10 = "DWord"
-$value10 = 1
+$value10 = 0
 
 $name11 = "LockedRetryCount"
 $type11 = "DWord"
@@ -74,20 +74,24 @@ $name14 = "ReAttachRetryCount"
 $type14 = "DWord"
 $value14 = 3
 
-New-Item -Path $path
-New-ItemProperty -Path $path -Name $name -PropertyType $type -Value $value
-New-ItemProperty -Path $path1 -Name $name1 -PropertyType $type1 -Value $value1
-New-ItemProperty -Path $path1 -Name $name2 -PropertyType $type2 -Value $value2
-New-ItemProperty -Path $path1 -Name $name3 -PropertyType $type3 -Value $value3
-New-ItemProperty -Path $path1 -Name $name4 -PropertyType $type4 -Value $value4
-New-ItemProperty -Path $path1 -Name $name5 -PropertyType $type5 -Value $value5
-New-ItemProperty -Path $path1 -Name $name6 -PropertyType $type6 -Value $value6
-New-ItemProperty -Path $path2 -Name $name7 -PropertyType $type7 -Value $value7
-New-ItemProperty -Path $path1 -Name $name8 -PropertyType $type8 -Value $value8
-New-ItemProperty -Path $path1 -Name $name9 -PropertyType $type9 -Value $value9
-New-ItemProperty -Path $path1 -Name $name10 -PropertyType $type10 -Value $value10
-New-ItemProperty -Path $path1 -Name $name11 -PropertyType $type11 -Value $value11
-New-ItemProperty -Path $path1 -Name $name12 -PropertyType $type12 -Value $value12
-New-ItemProperty -Path $path1 -Name $name13 -PropertyType $type13 -Value $value13
-New-ItemProperty -Path $path1 -Name $name14 -PropertyType $type14 -Value $value14
+# New-Item -Path $path
+# New-ItemProperty -Path $path -Name $name -PropertyType $type -Value $value
+# New-ItemProperty -Path $path1 -Name $name1 -PropertyType $type1 -Value $value1
+# New-ItemProperty -Path $path1 -Name $name2 -PropertyType $type2 -Value $value2
+# New-ItemProperty -Path $path1 -Name $name3 -PropertyType $type3 -Value $value3
+# New-ItemProperty -Path $path1 -Name $name4 -PropertyType $type4 -Value $value4
+# New-ItemProperty -Path $path1 -Name $name5 -PropertyType $type5 -Value $value5
+# New-ItemProperty -Path $path1 -Name $name6 -PropertyType $type6 -Value $value6
+# New-ItemProperty -Path $path2 -Name $name7 -PropertyType $type7 -Value $value7
+# New-ItemProperty -Path $path1 -Name $name8 -PropertyType $type8 -Value $value8
+# New-ItemProperty -Path $path1 -Name $name9 -PropertyType $type9 -Value $value9
+# New-ItemProperty -Path $path1 -Name $name10 -PropertyType $type10 -Value $value10
+# New-ItemProperty -Path $path1 -Name $name11 -PropertyType $type11 -Value $value11
+# New-ItemProperty -Path $path1 -Name $name12 -PropertyType $type12 -Value $value12
+# New-ItemProperty -Path $path1 -Name $name13 -PropertyType $type13 -Value $value13
+# New-ItemProperty -Path $path1 -Name $name14 -PropertyType $type14 -Value $value14
+
+Set-ItemProperty -Path $path1 -Name $name1 -Value $value1
+Set-ItemProperty -Path $path1 -Name $name8 -Value $value8
+Set-ItemProperty -Path $path1 -Name $name9 -Value $value9
 
